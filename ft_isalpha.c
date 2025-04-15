917d8fe7e16b28b4e2c4bc8ab8795b3c7a303c01
Lucio <lucio.surcotaype@outlook.com>
2025-04-15 15:57:34 +0300

    ft_isalpha changes

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,35 +6,28 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 10:47:55 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/15 15:21:14 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/15 15:56:30 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<stdio.h>
 #include	<ctype.h>
 
-int	ft_isalpha(char *str)
+int	ft_isalpha(char str)
 {
-	int	i;
-
-	i = 0;
-	if (str[0] == '\0')
+	if (str == '\0')
 	{
-		return(1);
+		return(0);
 	}
-	while (str[i] != '\0')
-	{
-		if ((str[i] < 65 || str[i] > 90) && (str[i] < 97 || str[i] > 122))
-		{
-			return (0);
-		}
-		i++;
+	if ((str < 65 || str > 90) && (str < 97 || str > 122))
+	{		
+		return (0);
 	}
-	return (1);
+	return (1024);
 }
 int	main(void)
 {
-	char	*str = 'onlywords';
+	char	str = 'A';
 	
 	printf("%i\n", isalpha(str));
 	printf("%i",ft_isalpha(str));

f5906ad49039cf7a071937a326ee2cef980773c2
Lucio <lucio.surcotaype@outlook.com>
2025-04-15 14:47:03 +0300

    third commit

--- ft_*.c
+++ ft_*.c
@@ -6,13 +6,27 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 10:47:55 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/15 11:17:57 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/15 14:46:05 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include	<stdio.h>
+#include	<ctype.h>
+
+int	ft_isalpha(char *str)
+{
+	
+}
+int	ft_isdigit(char *str)
+{
+	
+}
 int	main(void)
 {
-	int num2;
-	char abc;
-	return (0);
+	char	*str;
+
+	*str = 'onlywords';
+	//ft_isalpha(char);
+	printf(%n, isalpha(str));
+	return(0);
 }

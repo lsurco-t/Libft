303d39ffaa1251b0ffd5647eca178a338059341d
Lucio <lucio.surcotaype@outlook.com>
2025-04-16 20:01:38 +0300

    latest changes

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,34 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/16 11:24:24 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/16 11:54:46 by lsurco-t         ###   ########.fr       */
+/*   Created: 2025/04/16 19:49:50 by lsurco-t          #+#    #+#             */
+/*   Updated: 2025/04/16 19:49:50 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctype.h>
-#include <stdio.h>
+#include	<stdio.h>
+#include	<ctype.h>
 
-int	ft_isalnum(int c)
+int	ft_isascii(int	asc)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0'
-			&& c <= '9'))
-	{
-		return (1);
-	}
-	return (0);
+	if (asc >= 0 && asc <= 127)
+		return(1);
+	return(0);
 }
 
 int	main(void)
 {
-	int	c;
+	int	asc;
 
-	c = 97;
-	printf("Recreated function value: %i\n", ft_isalnum(c));
-	printf("Original function value: %i", isalnum(c));
-	return (0);
+	asc = 127;
+	printf("Recreated function value is: %i\n", ft_isascii(asc));
+	printf("Original function value is: %i", isascii(asc));
+	return(0);
 }

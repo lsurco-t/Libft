587b78e8d36a861a77a6f50fff66ca2ea64df26f
Lucio <lucio.surcotaype@outlook.com>
2025-04-16 11:55:57 +0300

    ft_isalnum

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-NAME =*.c
-CC = gcc
+CC = cc
 CFLAGS = -Wall -Wextra -Werror
+NAME = libft.a
 SRCS = ft_*.c
 RM = -rf
 
---
+++ ft_isalnum.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   isalnum.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 11:24:24 by lsurco-t          #+#    #+#             */
+/*   Updated: 2025/04/16 11:54:46 by lsurco-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	<stdio.h>
+#include	<ctype.h>
+
+int	ft_isalnum(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	int	c;
+
+	c = 65;
+	printf("Created function value: %i\n", ft_isalnum(c));
+	printf("Original function value: %i", isalnum(c));
+	return(0);
+}

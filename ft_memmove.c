c54c04e543d48286e32b679fd298166a43d3e4be
Lucio <lucio.surcotaype@outlook.com>
2025-04-17 14:27:11 +0300

    added functions

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/17 12:59:21 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/17 14:16:26 by lsurco-t         ###   ########.fr       */
+/*   Created: 2025/04/17 14:15:34 by lsurco-t          #+#    #+#             */
+/*   Updated: 2025/04/17 14:24:47 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
-	while (i < n)
+	while(i < n)
 	{
 		((char *)dest)[i] = ((char *)src)[i];
 		i++;
@@ -33,8 +33,8 @@ int	main(void)
 	char	str3[20] = "Stringfortest";
 	char	str4[20] = "test";
 
-	memcpy(str1, str2, 4);
-	ft_memcpy(str3, str4, 4);
+	memmove(str1, str2, 6);
+	ft_memmove(str3, str4, 6);
 	printf("String result after using function: %s\n", str1);
 	printf("String result after recreated function used: %s\n", str3);
 	return (0);

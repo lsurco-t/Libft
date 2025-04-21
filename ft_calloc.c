b3ebe1d489f25490498dcdd340c38f4717d02f0f
Lucio <lucio.surcotaype@outlook.com>
2025-04-21 20:02:29 +0300

    added calloc function

---
+++ ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 19:53:43 by lsurco-t          #+#    #+#             */
+/*   Updated: 2025/04/21 19:58:00 by lsurco-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	if(nmemb == 0)
+		return(void *ptr(malloc(1));
+
+}
+
+int	main(void)
+{
+
+	
+}

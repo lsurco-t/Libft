b8bfe3eae2682d884a53bde91f9e8910e27f4088
Lucio <lucio.surcotaype@outlook.com>
2025-04-21 19:28:32 +0300

    last atoi changes

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,48 +6,61 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:31:37 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/21 18:07:50 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/21 19:24:16 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-/*int	ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
-	char *str;
+	int	n;
+	int	i;
+	int	minus;
 
-	str = (char *)nptr;
-	if(str == "-2147483648")
+	n = 0;
+	i = 0;
+	minus = 1;
+	while (nptr[i] == ' ' || nptr[i] == '\f' || nptr[i] == '\n'
+		|| nptr[i] == '\r' || nptr[i] == '\t' || nptr[i] == '\v')
+		i++;
+	while (nptr[i] == '-' || nptr[i] == '+')
 	{
-		write(1, "-", 1);
-		write(1, "2147483648", 11);
+		if (nptr[i] == '-')
+			minus = minus * -1;
+		if (nptr[i + 1] == '-' || nptr[i + 1] == '+')
+			return (0);
+		i++;
 	}
-	return(0);
-}*/
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		n = (n * 10) + (nptr[i] - 48);
+		i++;
+	}
+	return (n * minus);
+}
 
 int	main(void)
 {
-	char *c;
-	int oa;
-	int	fa;
+	char	*c;
+	int		oa;
+	int		fa;
 
-	c = "2147483647";
+	c = "-2147483648964646654666";
 	oa = atoi(c);
 	fa = ft_atoi(c);
 	if (oa == fa)
 	{
-		printf("Same results!");
+		printf("Same results!\n");
 		printf("Expected: %i\n", atoi(c));
-		printf("Function result: %i", ft_atoi(c));
+		printf("Function result: %i\n", ft_atoi(c));
 	}
 	else
 	{
-		printf("Same results!");
+		printf("Different results!\n");
 		printf("Expected: %i\n", atoi(c));
-		printf("Function result: %i", ft_atoi(c));
+		printf("Function result: %i\n", ft_atoi(c));
 	}
-	return(0);
+	return (0);
 }
-

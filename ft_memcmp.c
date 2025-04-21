89b2a5b029a5d48d198f75e799a2c45186a2ed88
Lucio <lucio.surcotaype@outlook.com>
2025-04-21 13:32:40 +0300

    added functions

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,44 +6,45 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 11:22:07 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/21 11:46:11 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/21 13:28:14 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i;
-	unsigned char *ch1;
-	unsigned char *ch2;
+	size_t			i;
+	unsigned char	*ch1;
+	unsigned char	*ch2;
 
 	i = 0;
-	ch1 = (unsigned char *) s1;
-	ch2 = (unsigned char *) s2;
-
+	ch1 = (unsigned char *)s1;
+	ch2 = (unsigned char *)s2;
 	if (n == 0)
-		return(0);
+		return (0);
 	while (i < n)
 	{
 		if (ch1[i] != ch2[i] || ch1[i] == '\0' || ch2[i] == '\0')
 		{
 			return (ch1[i] - ch2[i]);
 		}
-	i++;
+		i++;
 	}
-	return(ch1[i] - ch2[i]);
+	return (0);
 }
 
 int	main(void)
 {
-	char	*str = "findhereacharz";
-	char	*str2 = "findherea";
-	size_t n;
-	
+	char	*str;
+	char	*str2;
+	size_t	n;
+
+	str = "findhereacharz";
+	str2 = "findhereaC";
 	n = 0;
-	while (n <= 10)
+	while (n <= 15)
 	{
 		if (memcmp(str, str2, n) == ft_memcmp(str, str2, n))
 		{
@@ -62,4 +63,3 @@ int	main(void)
 	}
 	return (0);
 }
-

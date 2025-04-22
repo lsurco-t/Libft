d272e51744a0d09c48de21d6aa45d9a1773f09d4
Lucio <lucio.surcotaype@outlook.com>
2025-04-22 12:58:57 +0300

    ft_substr.c changes

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 12:27:18 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/22 12:28:45 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/22 12:58:41 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 
 }
+
+int	main(void)
+[
+	char	*str1;
+	char 	*str2;
+	size_t	len;
+
+	str1 = "stringtocopy";
+
+
+	return (0);
+]

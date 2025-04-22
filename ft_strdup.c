4491a5bc2b710e3449b7ab0f0354bede71ff9711
Lucio <lucio.surcotaype@outlook.com>
2025-04-22 12:15:24 +0300

    ft_strdup changes and formatting

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,16 +6,57 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 22:39:34 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/21 22:40:39 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/22 12:14:41 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 char	*ft_strdup(const char *s)
 {
+	char	*str;
+	size_t	i;
+	size_t	len;
 
-
+	len = 0;
+	i = 0;
+	while (s[len])
+		len++;
+	str = (char *)malloc(len * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	return (str);
 }
 
+int	main(void)
+{
+	char	*str1;
+	char	*str2;
+	char	*str3;
+
+	str1 = "22     2";
+	str2 = strdup(str1);
+	str3 = ft_strdup(str1);
+	if (strcmp(str2, str3) == 0)
+	{
+		printf("Same Results!\n");
+		printf("Expected result: %s\n", str2);
+		printf("Function result: %s\n", str3);
+	}
+	else
+	{
+		printf("Different Results!\n");
+		printf("Expected result: %s\n", str2);
+		printf("Function result: %s\n", str3);
+	}
+	free(str2);
+	free(str3);
+	return (0);
+}

af4a2b018525b6d216bc2ca1bbbde6725994285f
Lucio <lucio.surcotaype@outlook.com>
2025-04-23 15:04:22 +0300

    itoa changes

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 10:19:59 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/23 10:20:59 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/23 15:03:39 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,37 @@
 
 char	*ft_itoa(int n)
 {
+	size_t	i;
+	char	*str;
+	size_t	len;
 
+	if (n > 2147483647 || n < -2147483648)
+		return (NULL);
+	len = ft_strlen((char *)n);
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = (n / 10) * (n + 48);
+		i++;
+	}
+	return (str);
 }
 
-int main(void)
+int	main(void)
 {
+	int		n;
+	char	*strn;
 
+	n = 34;
+	strn = ft_itoa(n);
+	if (strn == NULL)
+		printf("Invalid number!\n");
+	free(strn);
+	return (1);
+	printf("Valid number %s", strn);
+	free(strn);
+	return (0);
 }

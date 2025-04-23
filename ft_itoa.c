c8a669ecc52935587e0fc63c02f2614add5b174a
Lucio <lucio.surcotaype@outlook.com>
2025-04-23 19:13:26 +0300

    ft_itoa changes

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,39 +6,55 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 10:19:59 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/23 15:24:51 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/23 18:25:24 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static int ft_strnlen(int n)
 {
-	size_t	i;
-	char	*str;
-	size_t	len;
-	
-	if (n > 2147483647 || n < -2147483648)
-		return (NULL);
+	int len;
+
+	if (n == 0)
+		return (1);
 	if (n < 0)
-		n = n * -1;
-	len = 0;
-	while (n >= 10)
+		len = 1;
+	else
+		len = 0;	
+	while (n != 0)
 	{
-		n = n /10;
+		n = n / 10;
 		len++;
 	}
-	len = len + 1;
+	return(len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	size_t	len;
+	long int nb;
+		
+	nb = n;
+	len = ft_strnlen(nb);
 	str = malloc((len + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (i < len)
+	str[len] = '\0';
+	if (nb == 0)
+		str[0] = '0';
+	if (nb < 0)
 	{
-		str[i] = (n % 10) + (n + 48);
-		i++;
+		str[0] = '-';
+		nb = nb * -1;
 	}
-	str[i] = '\0';
+	while (nb > 0)
+	{
+		len--;
+		str[len] = (nb % 10) + 48;
+		nb = nb / 10;
+	}
 	return (str);
 }
 
@@ -47,13 +63,18 @@ int	main(void)
 	int		n;
 	char	*strn;
 
-	n = 2147483648;
-	strn = ft_itoa(n);
-	if (strn == NULL)
-		printf("Invalid number!\n");
-	free(strn);
-	return (1);
-	printf("Valid number %s", strn);
-	free(strn);
+	n = -568;
+	while (n <= -566)
+	{
+		strn = ft_itoa(n);
+		if (strn == NULL)
+		{
+			printf("Invalid number!\n");
+			return (1);
+		}
+		printf("Valid number %s\n\n", strn);
+		n++;
+		free(strn);
+	}
 	return (0);
 }

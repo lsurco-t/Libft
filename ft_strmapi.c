f25dcc6d678e2ae1560a613f41759a6ff9c4d81a
Lucio <lucio.surcotaype@outlook.com>
2025-04-23 23:40:19 +0300

    formatting changes

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,25 +6,25 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 22:32:23 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/23 23:27:17 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/23 23:29:19 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned 	int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	unsigned int	i;
-	char	*str;
-	size_t	len;
-		
+	char			*str;
+	size_t			len;
+
 	i = 0;
 	if (!s)
-		return(NULL);
+		return (NULL);
 	len = ft_strlen(s);
 	str = malloc((len + 1) * sizeof(char));
 	if (str == NULL)
-		return(NULL);
+		return (NULL);
 	while (s[i])
 	{
 		str[i] = f(i, s[i]);
@@ -32,20 +32,22 @@ char *ft_strmapi(char const *s, char (*f)(unsigned 	int, char))
 	}
 	return (str);
 }
-static char test_function(unsigned int i, char c)
+
+static char	test_function(unsigned int i, char c)
 {
-	(void)	i;
-	return(ft_toupper(c));
+	(void)i;
+	return (ft_toupper(c));
 }
 
-static char test_function2(unsigned int i, char c)
+static char	test_function2(unsigned int i, char c)
 {
-	(void)	i;
+	(void)i;
 	if (ft_isalpha(c))
-		return('1');
+		return ('1');
 	else
-		return('0');
+		return ('0');
 }
+
 int	main(void)
 {
 	char	*str1;
@@ -55,10 +57,8 @@ int	main(void)
 	str2 = ft_strmapi(str1, test_function);
 	printf("Result of function: %s\n", str2);
 	free(str2);
-
 	str2 = ft_strmapi(str1, test_function2);
 	printf("Result of function: %s\n", str2);
 	free(str2);
-
 	return (0);
 }

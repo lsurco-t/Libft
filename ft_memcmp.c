ed1f0114db744088d34800c1fb2eca4d621f7ff1
Lucio <lucio.surcotaype@outlook.com>
2025-04-24 22:58:43 +0300

    formatting ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 11:22:07 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/24 21:01:10 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/24 22:58:00 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,23 +37,22 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 int	main(void)
 {
-	char	str[] = {0,0,127,0};
-	char	str2[]= {0,0,42,0};
+	char	str[] = {0, 0, 127, 0};
+	char	str2[] = {0, 0, 42, 0};
 	size_t	n;
 
-	n = 2;
-		if (memcmp(str, str2, n) == ft_memcmp(str, str2, n))
-		{
-			printf("Same results!\n");
-			printf("Expected: %i\n", memcmp(str, str2, n));
-			printf("Delivered: %i\n\n", ft_memcmp(str, str2, n));
-		}
-		else
-		{
-			printf("Different results!\n");
-			printf("Expected: %i\n", memcmp(str, str2, n));
-			printf("Delivered: %i\n\n", ft_memcmp(str, str2, n));
-		}
+	n = 4;
+	if (memcmp(str, str2, n) == ft_memcmp(str, str2, n))
+	{
+		printf("Same results!\n");
+		printf("Expected: %i\n", memcmp(str, str2, n));
+		printf("Delivered: %i\n\n", ft_memcmp(str, str2, n));
+	}
+	else
+	{
+		printf("Different results!\n");
+		printf("Expected: %i\n", memcmp(str, str2, n));
+		printf("Delivered: %i\n\n", ft_memcmp(str, str2, n));
+	}
 	return (0);
 }
-

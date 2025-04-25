c44b1451e8135a547bd2e256984e181daedca93c
Lucio <lucio.surcotaype@outlook.com>
2025-04-25 08:18:57 +0300

    formatting changes to ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -15,9 +15,9 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	unsigned char	ch;
-	const	unsigned char *str;
+	size_t				i;
+	unsigned char		ch;
+	const unsigned char	*str;
 
 	i = 0;
 	ch = (unsigned char)c;

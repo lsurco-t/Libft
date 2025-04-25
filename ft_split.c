87d54b4e4ab94a413000d1de4ee36786fa94f393
Lucio <lucio.surcotaype@outlook.com>
2025-04-25 12:36:00 +0300

    changes to ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lsurco-t <lsurco-t@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 10:18:21 by lsurco-t          #+#    #+#             */
-/*   Updated: 2025/04/23 10:21:16 by lsurco-t         ###   ########.fr       */
+/*   Updated: 2025/04/25 12:19:25 by lsurco-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,46 @@
 
 char	**ft_split(char const *s, char c)
 {
+	char	*strout;
+	size_t	i;
+	size_t	len;
+	size_t	totallen;
 
+	len = ft_strlen(s);
+	i = 0;
+	while (s[i])
+	{
+		len++;
+		i++;
+	}
+	totallen = len;
+	strout = malloc ((totallen + 1) * sizeof(char));
+	if (strout == NULL)
+		return(NULL);
+	i = 0;
+	while (s[i])
+	{
+		strout[i] = s[i];
+		if (i < totallen)
+		{
+			i++;
+			strout[i] = c;
+		}
+	}
+	strout[i] = '\0';
+	return (*strout);
 }
 
 int	main(void)
 {
+	char	*str;
+	char	c;
+	char	**str_output;
 
+	str = "splitthisstring";
+	c = '_';
+	str_output = ft_split(str,c);
+	printf("Function result after split: %s", *str_output);
+	free(str_output);
+	return (0);	
 }
